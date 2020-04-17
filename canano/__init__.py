1cb02bdf91de57be60c98aff2b2515c9fab9afea
TJ Bruno <Tbruno25@gmail.com>
2020-04-17 11:45:01 -0700

    Update init

--- canano/__init__.py
+++ canano/__init__.py
@@ -1,6 +1,13 @@
-from . import pi, canano
+import pysocketcan
+from .canano import Can, Component, PinMap, GPIO
 
-for pin in pi.PinMap:
-    globals()[pin.name] = canano.Component(pin)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+for pin in PinMap:
+    if pin.is_input:
+        GPIO.setup(pin.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    else:
+        GPIO.setup(pin.pin, GPIO.OUT)
+    globals()[pin.name] = Component(pin)
 
-canano = canano.Can(pi.Interface())
+canano = Can()
